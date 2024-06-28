39cae40b4a59e5a3a050f32f6eeb14168bfaa9c7
SIMMY XAVIER <109429735+siim2mary@users.noreply.github.com>
2024-06-28 20:22:44 +0530

    Add files via upload

---
+++ Machine Learning/Assignments ML/Assignment 3 Machine learning basics jun 20/STREAMLIT PRACTICAL EXAM_JUN 28/my_app_exam.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title("MY STREAMLIT APP WITH IMAGE")
+st.header("The image")
+st.subheader("This is the image of MODI")
+st.write("THIS IS MY STREAMLIT APP WITH THE IMAGE OF MODI.I AM DEPLOYING IT WITH STREAMLIT")
+
+st.image("Modi.jpg", caption = "Deployment of Image of MODI using Streamlit", width =200)
+st.button("This Streamlit App I have created during Test")
+st.markdown("*streamlit* is used to **Deploy**")
