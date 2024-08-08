78d3313bcd933be6da0f43e253221601bfbfab46
SIMMY XAVIER <109429735+siim2mary@users.noreply.github.com>
2024-08-08 12:11:45 +0530

    Update gemini_bot8.py
    added back google_api_key

--- Mini Project2-Building LLM Chatbot_Gemini_Pro/Mini Project2-Building LLM Chatbot_Gemini_Pro/gemini_bot8.py
+++ Mini Project2-Building LLM Chatbot_Gemini_Pro/Mini Project2-Building LLM Chatbot_Gemini_Pro/gemini_bot8.py
@@ -15,9 +15,8 @@ st.set_page_config(
     layout="centered",  # Page layout option
 )
 
-#GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-# Retrieve the API key from Streamlit secrets
-GOOGLE_API_KEY = st.secrets["general"]["GOOGLE_API_KEY"]
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
 
 # Set up Google Gemini-Pro AI model
 gen_ai.configure(api_key=GOOGLE_API_KEY)
